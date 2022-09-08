8d32825a47062ced705e037430467a7630e058ed
takathanda538 <106585979+takathanda538@users.noreply.github.com>
2022-09-08 08:55:53 +0000

    [finish]非同期

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,25 +1,29 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!
-  
+
   def create
     @book = Book.find(params[:book_id])
+    @comments = @book.comments
     @comment = @book.comments.new(comment_params)
     @comment.user_id = current_user.id
-    if @comment.save
-      redirect_back(fallback_location: root_path) 
-    else
-      redirect_back(fallback_location: root_path) 
-    end
+    @comment.save
+    # if @comment.save
+    #   redirect_back(fallback_location: root_path)
+    # else
+    #   redirect_back(fallback_location: root_path)
+    # end
   end
-  
+
   def destroy
+    @book = Book.find(params[:book_id])
+    @comments = @book.comments
     Comment.find(params[:id]).destroy
-    redirect_back(fallback_location: root_path) 
+    # redirect_back(fallback_location: root_path)
   end
-  
+
   private
   def comment_params
     params.require(:comment).permit(:comment, :book_id)  #formにてpost_idパラメータを送信して、コメントへpost_idを格納するようにする必要がある。
   end
-  
+
 end
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,15 +2,17 @@ class FavoritesController < ApplicationController
   before_action :authenticate_user!
   
   def create
+    @book = Book.find(params[:book_id])
     @book_favorite = Favorite.new(user_id: current_user.id, book_id: params[:book_id])
     @book_favorite.save
-    redirect_back(fallback_location: root_path)
+    # redirect_back(fallback_location: root_path)
   end
   
   def destroy
+    @book = Book.find(params[:book_id])
     @book_favorite = Favorite.find_by(user_id: current_user.id, book_id: params[:book_id])
     @book_favorite.destroy
-    redirect_back(fallback_location: root_path)
+    # redirect_back(fallback_location: root_path)
   end
   
 end
